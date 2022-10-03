0c324572c47dd17f0e475be4d594b6baec18f4ed
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-10-03 11:48:29 +0300

    [feat] added product category

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -29,14 +29,22 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productService.getProductCategory(1));
-        context.setVariable("products", productService.getProductsForCategory(1));
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+
+//        context.setVariable("category", productService.getProductCategory(1));
+//        context.setVariable("category", productService.getProductCategory(2));
+//
+//
+//        context.setVariable("products", productService.getProductsForCategory(1));
+
+
+         // Alternative setting of the template context
+         Map<String, Object> params = new HashMap<>();
+         params.put("category", productCategoryDataStore.getAll());
+//         params.put("category", productCategoryDataStore.find(2));
+         params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+//         params.put("products", productDataStore.getBy(productCategoryDataStore.find(2)));
+
+         context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
     }
-
 }
