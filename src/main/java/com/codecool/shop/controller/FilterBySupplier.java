2e1dded6fe930c67549c20bf0484a000e8b92d5c
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-10-03 16:22:34 +0300

    [fix] fixed bug with filter by supplier

--- src/main/java/com/codecool/shop/controller/FilterBySupplier.java
+++ src/main/java/com/codecool/shop/controller/FilterBySupplier.java
@@ -7,11 +7,7 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
-import com.codecool.shop.service.ProductService;
-import com.google.gson.Gson;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -33,29 +29,21 @@ public class FilterBySupplier extends HttpServlet {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        Supplier supplier = null;
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        Map<String, Object> params = new HashMap<>();
-
         if(!req.getParameter("supplier").equals("")) {
             int supplierId = Integer.parseInt(req.getParameter("supplier"));
-            Supplier supplier = supplierDataStore.find(supplierId);
-
-            params.put("categories", productCategoryDataStore.getAll());
-            params.put("products", productDataStore.getBy(supplier));
-            params.put("suppliers", supplierDataStore.getAll());
-
-        } else if(!req.getParameter("category").equals("")) {
-            int categoryId = Integer.parseInt(req.getParameter("category"));
-            ProductCategory productCategory = productCategoryDataStore.find(categoryId);
-
-            params.put("categories", productCategoryDataStore.getAll());
-            params.put("products", productDataStore.getBy(productCategory));
-            params.put("suppliers", supplierDataStore.getAll());
-
+            supplier = supplierDataStore.find(supplierId);
         }
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("categories", productCategoryDataStore.getAll());
+        params.put("products", productDataStore.getBy(supplier));
+        params.put("suppliers", supplierDataStore.getAll());
+
         context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
     }
