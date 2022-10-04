3d16ee62cb2271137c5f933d0182590094698291
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-10-04 15:44:49 +0300

    [feat] created remove, decrease quantity, clear and size of cart methods

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -4,8 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    List<LineItem> items = new ArrayList<>();
+    List<LineItem> items;
 
+    public Cart() {
+        this.items = new ArrayList<>();
+    }
+
+    public List<LineItem> getItems () {
+        return items;
+    }
 
     public void addToCart(LineItem itemToAdd) {
         if(items.contains(itemToAdd)) {
@@ -18,6 +25,21 @@ public class Cart {
     }
 
     public void removeItem(LineItem itemToRemove) {
+        items.remove(itemToRemove);
+    }
 
+    public void decreaseQuantity(LineItem item) {
+        if(items.contains(item)){
+            LineItem existingItem = items.get(items.indexOf(item));
+            existingItem.setQuantity(existingItem.getQuantity() - 1);
+        }
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
+    public int sizeOfCart() {
+        return items.size();
     }
 }
