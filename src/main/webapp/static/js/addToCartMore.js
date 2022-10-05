bc2ab3eb270e6af733d6f5bb184a643de74777a2
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-10-05 19:27:17 +0300

    [feat] created fetch for add more products on card page and card page refactored

---
+++ src/main/webapp/static/js/addToCartMore.js
@@ -0,0 +1,32 @@
+const addMore = () => {
+
+    let addMoreButtons = document.querySelectorAll('[data-more-id]');
+
+    for (let btn of addMoreButtons) {
+        btn.addEventListener("click", async (e) => {
+            e.preventDefault();
+
+            let productId = btn.getAttribute("data-more-id");
+
+            const dataToBePosted = {
+                id: productId,
+                quantity: 1,
+            };
+            await fetch("/add-to-cart", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataToBePosted)
+            });
+
+            let input = document.getElementById('2');
+        });
+    }
+};
+
+const init = () => {
+    addMore();
+}
+
+init();
