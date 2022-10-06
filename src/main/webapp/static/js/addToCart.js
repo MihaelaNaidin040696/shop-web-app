5148c9421bbdc778acbc1cc47d12537d66469387
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-10-06 11:59:05 +0300

    [fix] short refactor

--- src/main/webapp/static/js/addToCart.js
+++ src/main/webapp/static/js/addToCart.js
@@ -20,37 +20,9 @@ const addToCart = () => {
         })
     }
 };
-//
-// const addMore = () => {
-//
-//     let addMoreButtons = document.querySelectorAll('[data-more-id]');
-//
-//     for (let btn of addMoreButtons) {
-//         btn.addEventListener("click", async (e) => {
-//             e.preventDefault();
-//
-//             let productId = btn.getAttribute("data-more-id");
-//
-//             const dataToBePosted = {
-//                 id: productId,
-//                 quantity: 1,
-//             };
-//             await fetch("/add-to-cart", {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify(dataToBePosted)
-//             });
-//
-//             let input = document.getElementById('2');
-//         });
-//     }
-// };
 
 const init = () => {
     addToCart();
-    // addMore();
 }
 
 init();
