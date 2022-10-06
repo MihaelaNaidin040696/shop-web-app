72a61565d136dfca0520f8eb0a0950f880b0a271
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-10-06 11:54:52 +0300

    [feat] save order details from html form

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -31,18 +31,33 @@ public class OrderController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductService productService = new ProductService();
 
-        String username = req.getParameter("name");
-        String billingAddress = req.getParameter("billingAddress");
-        String shippingAddress = req.getParameter("shippingAddress");
+        String firstName = req.getParameter("first-name");
+        String lastName = req.getParameter("last-name");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        String billingCountry = req.getParameter("billing-country");
+        String billingCity = req.getParameter("billing-city");
+        String billingZip = req.getParameter("billing-zip");
+        String billingAddress = req.getParameter("billing-address");
+        String shippingCountry = req.getParameter("shipping-country");
+        String shippingCity = req.getParameter("shipping-city");
+        String shippingZip = req.getParameter("shipping-zip");
+        String shippingAddress = req.getParameter("shipping-address");
 
-        productService.getOrderDao().addInformationFromUser(username);
+        productService.getOrderDao().addInformationFromUser(firstName);
+        productService.getOrderDao().addInformationFromUser(lastName);
+        productService.getOrderDao().addInformationFromUser(email);
+        productService.getOrderDao().addInformationFromUser(phone);
+        productService.getOrderDao().addInformationFromUser(billingCountry);
+        productService.getOrderDao().addInformationFromUser(billingCity);
+        productService.getOrderDao().addInformationFromUser(billingZip);
         productService.getOrderDao().addInformationFromUser(billingAddress);
+        productService.getOrderDao().addInformationFromUser(shippingCountry);
+        productService.getOrderDao().addInformationFromUser(shippingCity);
+        productService.getOrderDao().addInformationFromUser(shippingZip);
         productService.getOrderDao().addInformationFromUser(shippingAddress);
 
-        System.out.println(productService.getOrderDao().getInfoUser());
-
         productService.getOrderDao().clearOrder();
-        resp.sendRedirect("index.html");
-
+        resp.sendRedirect("/payment");
     }
 }
